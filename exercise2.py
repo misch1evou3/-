0ab56d096a80ba266b2fd63616a341575c1d3754
misch1evou3 <144317549+misch1evou3@users.noreply.github.com>
2023-10-18 16:21:06 +0800

    Add files via upload

---
+++ exercise2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 15:47:01 2023
+
+@author: User
+"""
+
+name = ['Graham Chapman', 'John Cleese', 'Terry Gilliam', 'Eric Idle', 'Terry Jones']
+score = [70, 80, 90, 20, 100]
+print({name[i]:score[i] for i in range(5)})
+
